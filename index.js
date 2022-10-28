7106c43ec894316142265a7f2b25ab456196e8be
Karanjot Singh <ksingh@megger.net>
2022-10-28 02:03:56 -0500

    ...

--- index.js
+++ index.js
@@ -2,19 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const cors = require('cors')
-var whitelist = ['http://localhost:3000', 'https://hr-management-4a24c.web.app/']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-// Then pass them to cors:
-app.use(cors(corsOptions));
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send({test:123})
