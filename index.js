4800c582f265c83ab505e028b4a9ee326f755493
Karanjot Singh <ksingh@megger.net>
2022-10-28 01:49:49 -0500

    ...

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 8080;
 // const cors = require('cors')
 // var whitelist = ['http://localhost:3000']
 // var corsOptions = {
