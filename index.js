2c164a1b60d74b0f307c07a65b60d7830fb22bc3
Karanjot Singh <ksingh@megger.net>
2022-10-28 00:29:29 -0500

    ...

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+const cors = require('cors')
+var whitelist = ['http://localhost:3000']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+// Then pass them to cors:
+app.use(cors(corsOptions));
+
+app.get('/', (req, res) => {
+    res.send({test:123})
+});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
